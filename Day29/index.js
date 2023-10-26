1478488b3bf6be39415d863bdc828f4618fdc86b
Jayavardhan01 <kondajayavardhan2401@gmail.com>
2023-10-26 12:28:55 +0530

    adding files

---
+++ Day29/index.js
@@ -0,0 +1,28 @@
+class studentinfo{
+    constructor(a,b,c){
+        this.Name=a,
+        this.Branch=b,
+        this.rollno=c
+    }
+}
+
+const std1=new studentinfo("Sai","IT",19);
+console.log(std1);
+const std2=new studentinfo("Vardhan","IT",20);
+console.log(std2);
+const std3=new studentinfo("Bhanu","IT",07);
+console.log(std3);
+const std4=new studentinfo("Roopesh","IT",32);
+console.log(std4);
+const std5=new studentinfo("Venkatesh","It",24);
+console.log(std5);
+const std6=new studentinfo("Vishnu","It",04);
+console.log(std6);
+const std7=new studentinfo("Naveen","IT",30);
+console.log(std7);
+const std8=new studentinfo("Venu","CIVIL",26);
+console.log(std8);
+const std9=new studentinfo("Gopal","IT",14);
+console.log(std9);
+const std10=new studentinfo("Sai pavan ","IT",26);
+console.log(std10);
