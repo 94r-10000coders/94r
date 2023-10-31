b7f75d95a2a5e117019f0263f4e7585ebd83b60d
Jayavardhan01 <kondajayavardhan2401@gmail.com>
2023-10-31 18:17:16 +0530

    adding files

--- Day32/index.js
+++ Day32/index.js
@@ -1,28 +1,127 @@
-class studentinfo{
-    constructor(a,b,c){
-        this.Name=a,
-        this.Branch=b,
-        this.rollno=c
+console.log("Encapsulation with Getters and Setters");
+class BankAccount{
+  
+    SetAccountNo(a){
+         this.accountNumber=a;
+    }
+    Setbalance(b){
+         this.balance=b;
+    }
+    getAccountNumber(){
+        console.log(`Account Number: ${this.accountNumber}`);
+    }
+    getBalance(){
+        console.log(`Balance: ${this.balance}`);
+    }
+    deposit(amount){
+        this.balance+=amount;
+        console.log(`Current Balance is: ${this.balance}`);
+    }
+    withdraw(amount){
+        if(amount<this.balance){
+            console.log(`Withdrawn Amount is: ${amount}`);
+            console.log(`current balance is: ${this.balance-amount}`);
+        }
+        
     }
 }
+let obj1=new BankAccount();
+obj1.SetAccountNo(123456);
+obj1.Setbalance(800000);
+obj1.getAccountNumber();
+obj1.getBalance();
+obj1.deposit(25000);
+obj1.withdraw(5000);
 
-const std1=new studentinfo("Sai","IT",19);
-console.log(std1);
-const std2=new studentinfo("Vardhan","IT",20);
-console.log(std2);
-const std3=new studentinfo("Bhanu","IT",07);
-console.log(std3);
-const std4=new studentinfo("Roopesh","IT",32);
-console.log(std4);
-const std5=new studentinfo("Venkatesh","It",24);
-console.log(std5);
-const std6=new studentinfo("Vishnu","It",04);
-console.log(std6);
-const std7=new studentinfo("Naveen","IT",30);
-console.log(std7);
-const std8=new studentinfo("Venu","CIVIL",26);
-console.log(std8);
-const std9=new studentinfo("Gopal","IT",14);
-console.log(std9);
-const std10=new studentinfo("Sai pavan ","IT",26);
-console.log(std10);
+console.log("Private Properties:TemperatureConverter");
+    class TemperatureConverter{
+
+        setcelcius(value){
+            this.val=value;
+        }
+        getCelcius(){
+            console.log(`Given Celcius value: ${this.val}`);
+        }
+        convertToFahrenheit(){
+           console.log(`In Farenhiet: ${this.val*(9/5)+32}`);
+        }
+        convertToKelvin(){
+            console.log(`In Kelvin: ${this.val+273.15}`);
+        }
+    }
+    const con1=new TemperatureConverter();
+    con1.setcelcius(28);
+    con1.getCelcius();
+    con1.convertToFahrenheit();
+    con1.convertToKelvin();
+
+
+    console.log("Static method and inheritance");
+    class MathOperations{
+        constructor(){
+            
+        }
+        static square(number){
+            var num1=number*number;
+            console.log("square",num1);
+        }
+    }
+    class AdvancedMathOperations extends MathOperations{
+        constructor(){
+            super();
+        }
+        static cube(number){
+            var num=number*number*number;
+            console.log("cube",num);
+        }
+    }
+    obj=new AdvancedMathOperations();
+    MathOperations.square(2);
+    AdvancedMathOperations.cube(2);
+
+
+
+
+  console.log("Inheritance and Method Overriding:");
+    class shape{
+        constructor(){
+            console.log(`Area and perimeters of`);
+        }
+        calculateArea(){
+            console.log(`override method `);
+        }
+        calculatePerimeter(){
+            console.log(`override method`);
+        }
+    }
+    class circle extends shape{
+        constructor(){
+            super();
+            console.log(` circle :-`);
+        }
+        calculateArea(r){
+            console.log("Area:",1.22*r*r);
+        }
+        calculatePerimeter(r){
+            console.log("Perimeter:",2*3.14*r);
+        }
+    }
+    class square extends shape{
+        constructor(){
+            super();
+            console.log(` square :-`);
+        }
+        calculateArea(s){
+            console.log("Area",s*s);
+        }
+        calculatePerimeter(s){
+            console.log("Perimeter:",4*s);
+        }
+    }
+
+    const obje2=new circle();
+    obje2.calculateArea(4);
+    obje2.calculatePerimeter(8);
+    const obje4=new square();
+    obje4.calculateArea(4);
+    obje4.calculatePerimeter(8);
